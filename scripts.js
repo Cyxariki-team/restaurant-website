7bb54131b50c5bed9a1222182858bc396063ffa6
Jefrex1 <oorest06@gmail.com>
2025-01-18 13:37:20 +0200

    d

--- scripts.js
+++ scripts.js
@@ -1,3 +1,7 @@
-function togglePopup(){
+function toggle_login_popup(){
     document.getElementById("popup_1").classList.toggle("active");
+}
+
+function toggle_registration_popup(){
+    document.getElementById("popup_2").classList.toggle("active");
 }
