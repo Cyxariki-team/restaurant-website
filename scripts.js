7c0a9537a6463c55f5ab73c4f136efbb217fb4af
Jefrex1 <oorest06@gmail.com>
2025-01-19 10:46:43 +0200

    about us popup, info copy t the clipboard

--- scripts.js
+++ scripts.js
@@ -1,7 +1,20 @@
 function toggle_login_popup(){
-    document.getElementById("popup_1").classList.toggle("active");
+    document.getElementById("popup_login").classList.toggle("active");
 }
 
 function toggle_registration_popup(){
-    document.getElementById("popup_2").classList.toggle("active");
+    document.getElementById("popup_reg").classList.toggle("active");
+}
+
+function toggle_aboutus_popup(){
+    document.getElementById("popup_about").classList.toggle("active");
+}
+
+function toggle_popup(popup){
+    document.getElementById(popup).classList.toggle("active");
+}
+
+function copy_on_clipboard(a){
+    var CopyText = document.getElementById(a)
+    navigator.clipboard.writeText(CopyText.textContent);
 }
