cecc2897111341afca5f80a80dcadd78c896cd53
Andriy2444 <andriy241179@gmail.com>
2025-01-23 21:04:17 +0200

    Add baner

--- static/js/scripts.js
+++ static/js/scripts.js
@@ -3,6 +3,40 @@ vers = 'version 0.3.6';
 console.log(vers);
 document.getElementById("VERSION").textContent=vers;
 
+const slides = document.querySelector('.slides');
+const prevButton = document.getElementById('prev');
+const nextButton = document.getElementById('next');
+const totalSlides = document.querySelectorAll('.slide').length;
+let currentIndex = 0;
+
+function updateButtons() {
+    // Buttons are always enabled in a looped slider
+}
+
+function showSlide(index) {
+    const slideWidth = document.querySelector('.slider-container').clientWidth;
+    slides.style.transform = `translateX(-${index * slideWidth}px)`;
+    currentIndex = index;
+}
+
+prevButton.addEventListener('click', () => {
+    if (currentIndex === 0) {
+        showSlide(totalSlides - 1);
+    } else {
+        showSlide(currentIndex - 1);
+    }
+});
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex === totalSlides - 1) {
+        showSlide(0);
+    } else {
+        showSlide(currentIndex + 1);
+    }
+});
+
+updateButtons();
+
 function toggle_popup(popup, closedby){
     document.getElementById(popup).classList.toggle("active");
     console.log(popup, " closed by: ", closedby);
@@ -91,41 +125,6 @@ function searchProducts() {
             console.error("Помилка:", error);
         });
 }
-
-const slides = document.querySelector('.slides');
-        const prevButton = document.getElementById('prev');
-        const nextButton = document.getElementById('next');
-        const totalSlides = document.querySelectorAll('.slide').length;
-        let currentIndex = 0;
-
-        function updateButtons() {
-            // Buttons are always enabled in a looped slider
-        }
-
-        function showSlide(index) {
-            const slideWidth = document.querySelector('.slider-container').clientWidth;
-            slides.style.transform = `translateX(-${index * slideWidth}px)`;
-            currentIndex = index;
-        }
-
-        prevButton.addEventListener('click', () => {
-            if (currentIndex === 0) {
-                showSlide(totalSlides - 1);
-            } else {
-                showSlide(currentIndex - 1);
-            }
-        });
-
-        nextButton.addEventListener('click', () => {
-            if (currentIndex === totalSlides - 1) {
-                showSlide(0);
-            } else {
-                showSlide(currentIndex + 1);
-            }
-        });
-
-        // Initialize
-        updateButtons();
 
 
 /*<button id="product.name" class="test_product" onclick="toggle_popup('popup_{{product.name}}'), 'EL PROBLEMO'">
